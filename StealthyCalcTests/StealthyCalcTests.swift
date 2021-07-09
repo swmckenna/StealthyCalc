2a3c171298ea2a7c829f322fe0eda8cdfaabfb96
Stephen McKenna <stephen@Stephens-MacBook-Pro.local>
2021-07-09 19:09:12 -0400

    adding tests

--- StealthyCalcTests/StealthyCalcTests.swift
+++ StealthyCalcTests/StealthyCalcTests.swift
@@ -9,25 +9,25 @@ import XCTest
 @testable import StealthyCalc
 
 class StealthyCalcTests: XCTestCase {
+    var sut: NumberCruncher!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = NumberCruncher()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testNumberCruncherMath() {
+        // 9 / 3 = 3
+        sut.setOperand(9)
+        sut.performOperation("÷")
+        sut.setOperand(3)
+        sut.performOperation("=")
+        XCTAssertEqual(sut.result, 3.0)
     }
 
 }
