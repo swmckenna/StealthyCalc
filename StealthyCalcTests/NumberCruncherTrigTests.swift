b908da1d8bc5ad2dead7406f9dc2825e68ca1fc9
Stephen McKenna <stephen@Stephens-MacBook-Pro.local>
2021-09-26 00:38:37 -0400

    changed trig functions with better understanding, added inv sin and inv cos tests

--- StealthyCalcTests/NumberCruncherTrigTests.swift
+++ StealthyCalcTests/NumberCruncherTrigTests.swift
@@ -21,6 +21,7 @@ class NumberCruncherTrigTests: XCTestCase {
     }
     
     //MARK: RADIANS TESTS
+    //MARK: BASIC
     func testSinRadians() {
         sut.performOperation("π")
         sut.performOperation("sinRAD")
@@ -187,6 +188,103 @@ class NumberCruncherTrigTests: XCTestCase {
         result = sut.evaluate()
         XCTAssertEqual(result.result!, 0, accuracy: 0.000000000000001)
         XCTAssertEqual(result.expressionString, "tan(0)")
+    }
+    
+    //MARK: INVERSE
+    func testSin⁻¹Radians() {
+        sut.setOperand(0)
+        sut.performOperation("sin⁻¹RAD")
+        var result = sut.evaluate()
+        XCTAssertEqual(result.result!, 0, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(0)")
+        
+        sut.setOperand(0.5)
+        sut.performOperation("sin⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/6, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(0.5)")
+        
+        sut.setOperand(-1)
+        sut.performOperation("sin⁻¹RAD")
+        sut.performOperation("=")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, -Double.pi/2, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(-1)")
+        
+        sut.clear()
+        sut.setOperand(2)
+        sut.performOperation("²√ₓ")
+        sut.performOperation("¹/ₓ")
+        sut.performOperation("sin⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/4, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(√2⁻¹)")
+        
+        sut.clear()
+        sut.setOperand(3)
+        sut.performOperation("²√ₓ")
+        sut.performOperation("÷")
+        sut.setOperand(2)
+        sut.performOperation("ᐩ/˗")
+        sut.performOperation("=")
+        sut.performOperation("sin⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, -Double.pi/3, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(√3÷˗(2))")
+        
+        sut.setOperand(1)
+        sut.performOperation("sin⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/2, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "sin⁻¹(1)")
+    }
+    
+    func testCos⁻¹Radians() {
+        sut.setOperand(0)
+        sut.performOperation("cos⁻¹RAD")
+        var result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/2, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(0)")
+        
+        sut.setOperand(0.5)
+        sut.performOperation("cos⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/3, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(0.5)")
+        
+        sut.setOperand(-1)
+        sut.performOperation("cos⁻¹RAD")
+        sut.performOperation("=")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(-1)")
+        
+        sut.clear()
+        sut.setOperand(2)
+        sut.performOperation("²√ₓ")
+        sut.performOperation("¹/ₓ")
+        sut.performOperation("cos⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, Double.pi/4, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(√2⁻¹)")
+        
+        sut.clear()
+        sut.setOperand(3)
+        sut.performOperation("²√ₓ")
+        sut.performOperation("÷")
+        sut.setOperand(2)
+        sut.performOperation("ᐩ/˗")
+        sut.performOperation("=")
+        sut.performOperation("cos⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, 5*Double.pi/6, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(√3÷˗(2))")
+        
+        sut.setOperand(1)
+        sut.performOperation("cos⁻¹RAD")
+        result = sut.evaluate()
+        XCTAssertEqual(result.result!, 0, accuracy: 0.000000000000001)
+        XCTAssertEqual(result.expressionString, "cos⁻¹(1)")
     }
     
     
