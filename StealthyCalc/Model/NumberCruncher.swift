0cb376048050d216d005d3f7745d2ca1aa20ce2d
Stephen McKenna <stephen@Stephens-MacBook-Pro.local>
2021-09-28 17:30:29 -0400

    refactored

--- StealthyCalc/Model/NumberCruncher.swift
+++ StealthyCalc/Model/NumberCruncher.swift
@@ -38,7 +38,7 @@ struct NumberCruncher {
     private enum Operation {
         case constant(Double)
         case nullary(NullaryFunction)
-        case unary(UnaryMathFunction, UnaryStringFunction, RepeatsOnEquals)
+        case unary(UnaryMathFunction, UnaryStringFunction, UnaryStringFunction?, RepeatsOnEquals)
         case binary (BinaryMathFunction, BinaryStringFunction?, Precedence, RepeatsOnEquals)
         case equals
     }
@@ -82,44 +82,44 @@ struct NumberCruncher {
         "e": Operation.constant(M_E),
         "π": Operation.constant(Double.pi),
         //
-        "ᐩ/˗": Operation.unary({ -$0 }, { $0.isJustANumber ? "˗\($0)" : "˗(\($0))" }, false),
-        "%": Operation.unary({ $0 / 100 }, { $0.isJustANumber ? "\($0)%" : "(\($0))%" }, false),
-        "х²": Operation.unary({ $0 * $0 }, { $0.isJustANumber ? "\($0)²" : "(\($0))²" }, true),
-        "х³": Operation.unary({ $0 * $0 * $0 }, { $0.isJustANumber ? "\($0)³" : "(\($0))³"}, true),
-        "eˣ": Operation.unary({ pow(M_E, $0) }, { $0.isJustANumber ? "e^\($0)" : "e^(\($0))" }, true),
-        "10ˣ": Operation.unary({ pow(10, $0) }, { $0.isJustANumber ? "10^\($0)" : "10^(\($0))" }, false),
-        "2ˣ": Operation.unary({ pow(2, $0) }, { $0.isJustANumber ? "2^\($0)" : "2^(\($0))" }, false),
-        "¹/ₓ": Operation.unary({ 1/$0 }, { $0.isJustANumber ? "\($0)⁻¹" : "(\($0))⁻¹" }, false),
-        "²√ₓ": Operation.unary({ sqrt($0) }, { $0.isJustANumber ? "√\($0)" : "√(\($0))" }, true),
-        "³√ₓ": Operation.unary({ pow($0, 1/3) }, { $0.isJustANumber ? "³√\($0)" : "³√(\($0))" }, true),
-        "㏑": Operation.unary({ log($0) } , { "㏑(\($0))" }, false),
-        "㏒₁₀": Operation.unary({ log10($0) }, { "㏒₁₀(\($0))" }, false),
-        "㏒₂": Operation.unary({ log2($0) }, { "㏒₂(\($0))" }, false),
+        "ᐩ/˗": Operation.unary({ -$0 }, { "˗(\($0))" },  { "˗\($0)" }, false),
+        "%": Operation.unary({ $0 / 100 },  { "(\($0))%" }, { "\($0)%" }, false),
+        "х²": Operation.unary({ $0 * $0 }, { "(\($0))²" }, { "\($0)²" }, true),
+        "х³": Operation.unary({ $0 * $0 * $0 }, { "(\($0))³"}, { "\($0)³" }, true),
+        "eˣ": Operation.unary({ pow(M_E, $0) }, { "e^(\($0))" }, { "e^\($0)" }, true),
+        "10ˣ": Operation.unary({ pow(10, $0) }, { "10^(\($0))" }, {"10^\($0)" }, false),
+        "2ˣ": Operation.unary({ pow(2, $0) }, { "2^(\($0))" }, { "2^\($0)" }, false),
+        "¹/ₓ": Operation.unary({ 1/$0 }, { "(\($0))⁻¹" }, { "\($0)⁻¹" }, false),
+        "²√ₓ": Operation.unary({ sqrt($0) }, { "√(\($0))" }, { "√\($0)" }, true),
+        "³√ₓ": Operation.unary({ pow($0, 1/3) }, { "³√(\($0))" }, { "³√\($0)" }, true),
+        "㏑": Operation.unary({ log($0) } , { "㏑(\($0))" }, nil, false),
+        "㏒₁₀": Operation.unary({ log10($0) }, { "㏒₁₀(\($0))" }, nil, false),
+        "㏒₂": Operation.unary({ log2($0) }, { "㏒₂(\($0))" }, nil, false),
         "х!": Operation.unary({ op in
             guard let i = Int(exactly: op), i >= 0 else { return Double.nan}
             if i == 0 { return 1 }
             return (1...i).map(Double.init).reduce(1.0, *)
-        }, { $0.isJustANumber ? "\($0)!" : "(\($0))!" }, false),
+        }, { "(\($0))!" }, {"\($0)!" }, false),
         //RADIANS
-        "sinRAD": Operation.unary({ sin($0).roundTo15Places() }, { "sin(\($0))" }, false),
-        "cosRAD": Operation.unary({ cos($0).roundTo15Places() }, { "cos(\($0))" }, false),
-        "tanRAD": Operation.unary({ sin($0).roundTo15Places()/cos($0).roundTo15Places() }, { "tan(\($0))" }, false),
-        "sin⁻¹RAD": Operation.unary({ asin($0) }, { "sin⁻¹(\($0))"}, false),
-        "cos⁻¹RAD": Operation.unary({ acos($0) }, { "cos⁻¹(\($0))"}, false),
-        "tan⁻¹RAD": Operation.unary({ atan($0) }, { "tan⁻¹(\($0))"}, false),
-        "sinh": Operation.unary({ sinh($0) }, { "sinh(\($0))" }, false),
-        "cosh": Operation.unary({ cosh($0) }, { "cosh(\($0))" }, false),
-        "tanh": Operation.unary({ tanh($0) }, { "tanh(\($0))" }, false),
-        "sinh⁻¹": Operation.unary({ asinh($0) }, { "sinh⁻¹(\($0))"}, false),
-        "cosh⁻¹": Operation.unary({ acosh($0) }, { "cosh⁻¹(\($0))"}, false),
-        "tanh⁻¹": Operation.unary({ atanh($0) }, { "tanh⁻¹(\($0))"}, false),
+        "sinRAD": Operation.unary({ sin($0).roundTo15Places() }, { "sin(\($0))" }, nil, false),
+        "cosRAD": Operation.unary({ cos($0).roundTo15Places() }, { "cos(\($0))" }, nil, false),
+        "tanRAD": Operation.unary({ sin($0).roundTo15Places()/cos($0).roundTo15Places() }, { "tan(\($0))" }, nil, false),
+        "sin⁻¹RAD": Operation.unary({ asin($0) }, { "sin⁻¹(\($0))"}, nil, false),
+        "cos⁻¹RAD": Operation.unary({ acos($0) }, { "cos⁻¹(\($0))"}, nil, false),
+        "tan⁻¹RAD": Operation.unary({ atan($0) }, { "tan⁻¹(\($0))"}, nil, false),
+        "sinh": Operation.unary({ sinh($0) }, { "sinh(\($0))" }, nil, false),
+        "cosh": Operation.unary({ cosh($0) }, { "cosh(\($0))" }, nil, false),
+        "tanh": Operation.unary({ tanh($0) }, { "tanh(\($0))" }, nil, false),
+        "sinh⁻¹": Operation.unary({ asinh($0) }, { "sinh⁻¹(\($0))"}, nil, false),
+        "cosh⁻¹": Operation.unary({ acosh($0) }, { "cosh⁻¹(\($0))"}, nil, false),
+        "tanh⁻¹": Operation.unary({ atanh($0) }, { "tanh⁻¹(\($0))"}, nil, false),
         //DEGREES
-        "sin": Operation.unary({ sin($0*Double.pi/180) }, { "sin(\($0)°)" }, false),
-        "cos": Operation.unary({ cos($0*Double.pi/180) }, { "cos(\($0)°)" }, false),
-        "tan": Operation.unary({ __sinpi($0/180)/__cospi($0/180) }, { "tan(\($0)°)" }, false),
-        "sin⁻¹": Operation.unary({ asin($0)/Double.pi*180 }, { "sin⁻¹(\($0))"}, false),
-        "cos⁻¹": Operation.unary({ acos($0)/Double.pi*180 }, { "cos⁻¹(\($0))"}, false),
-        "tan⁻¹": Operation.unary({ atan($0)/Double.pi*180 }, { "tan⁻¹(\($0))"}, false),
+        "sin": Operation.unary({ sin($0*Double.pi/180) }, { "sin(\($0)°)" }, nil, false),
+        "cos": Operation.unary({ cos($0*Double.pi/180) }, { "cos(\($0)°)" }, nil, false),
+        "tan": Operation.unary({ __sinpi($0/180)/__cospi($0/180) }, { "tan(\($0)°)" }, nil, false),
+        "sin⁻¹": Operation.unary({ asin($0)/Double.pi*180 }, { "sin⁻¹(\($0))"}, nil, false),
+        "cos⁻¹": Operation.unary({ acos($0)/Double.pi*180 }, { "cos⁻¹(\($0))"}, nil, false),
+        "tan⁻¹": Operation.unary({ atan($0)/Double.pi*180 }, { "tan⁻¹(\($0))"}, nil, false),
         //
         "ʸ√ₓ": Operation.binary({ pow($0, 1/$1) }, { "\($1)√\($0)" }, 2, true),
         "xʸ": Operation.binary({ pow($0, $1) }, { "\($0)^\($1)" }, 2, true),
@@ -205,7 +205,7 @@ struct NumberCruncher {
                     let function = nullaryFunction()
                     cache = (accumulator: function.result, expressionAccumulator: function.stringResult)
                     
-                case .unary(let mathFunction, let stringFunction, let repeats):
+                case .unary(let mathFunction, let complexStringFunction, let simpleStringFunction, let repeats):
                     if let accumulator = cache.accumulator {
                         let mathResult = mathFunction(accumulator)
                         if mathResult.isInfinite {
@@ -219,9 +219,11 @@ struct NumberCruncher {
                         if repeats {
                             operationOfRecord = (symbol, nil)
                         }
-//                        if stringFunction == nil {
-//                            stringFunction = { "\(symbol)(\($0))" }
-//                        }
+                        var stringFunction: UnaryStringFunction {
+                        //decide which string function to use based on if exp accumulator is a simply a number or a more complex expression
+                            guard simpleStringFunction != nil else { return complexStringFunction }
+                            return cache.expressionAccumulator!.isJustANumber ? simpleStringFunction! : complexStringFunction
+                        }
                         cache.expressionAccumulator = stringFunction(cache.expressionAccumulator!)
                     } else if symbol == "ᐩ/˗" {
                         negativeIsPending = !negativeIsPending
@@ -251,7 +253,7 @@ struct NumberCruncher {
                     if let opOfRecord = operationOfRecord,
                        let op = operationsDict[opOfRecord.symbol] {
                         switch op {
-                        case .unary(_, _, _):
+                        case .unary(_, _, _, _):
                             performOperation(opOfRecord.symbol)
                         case .binary(let math, let string, let prec, _):
                             if let firstOperand = cache.accumulator,
